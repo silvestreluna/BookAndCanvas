eaa4293c3718cd63d65ab49739ab08b70e973e9c
Silvestre Luna <slvtrluna@gmail.com>
2019-11-25 07:52:50 -0600

    added modal with title description, price

--- bookandcanvas.ui/src/components/NewArtWork/NewArtWork.js
+++ bookandcanvas.ui/src/components/NewArtWork/NewArtWork.js
@@ -1,22 +1,57 @@
 import React from 'react';
-import { Modal } from 'reactstrap';
+import { Modal, 
+        ModalBody, 
+        ModalFooter, 
+        Form, 
+        FormGroup, 
+        Label, 
+        Input } from 'reactstrap';
 
 import './NewArtWork.scss';
 
 class NewArtWork extends React.Component {
   state = {
-    modalState: false,
+    showingModal: false,
   }
 
   toggleModal = () => {
-    this.setState({ modalState: !modalState });
+    this.setState({ showingModal: !this.state.showingModal});
   }
 
   render() {
+    const modal = this.state.showingModal;
     return (
       <div className="NewArtWork">
         <h1>Hi</h1>
-        {console.error(this.state.modalState)}
+        <button onClick={this.toggleModal}>press</button>
+        {console.log(this.state.showingModal)}
+        <Modal isOpen={modal} centered={true} >
+          <ModalBody>
+            <Form>
+              <FormGroup>
+                <Label for="productTitle"></Label>
+                <Input placeholder="TITLE"/>
+              </FormGroup>
+              <FormGroup>
+                <Label for="productDescription"></Label>
+                <Input type="textarea" id="productDescription" placeholder="DESCRIPTION"/>
+              </FormGroup>
+              <div>
+                <FormGroup>
+                  <Label for="price"></Label>
+                  <Input placeholder="PRICE"/>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="typeCategory">SERVICE TYPE</Label>
+                  <Input type="select" name="type-selection" id="typeCategory"/>
+                </FormGroup>
+              </div>
+            </Form>
+          </ModalBody>
+          <ModalFooter>
+            <button onClick={this.toggleModal}>Close</button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
   }
