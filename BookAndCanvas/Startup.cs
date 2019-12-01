930b3f2465feaef4cfcda47d8eb6f53c4b0878d9
saul3d <saul3d@yahoo.com>
2019-12-01 15:30:06 -0600

    startup.cs has changed somehow.

--- BookAndCanvas/Startup.cs
+++ BookAndCanvas/Startup.cs
@@ -27,6 +27,14 @@ namespace BookAndCanvas
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
             services.AddScoped<IProductRepository, ProductRepo>();
         }
 
@@ -38,10 +46,12 @@ namespace BookAndCanvas
                 app.UseDeveloperExceptionPage();
             }
             else
-            {
+            {   
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("MyPolicy");
 
             app.UseHttpsRedirection();
             app.UseMvc();
