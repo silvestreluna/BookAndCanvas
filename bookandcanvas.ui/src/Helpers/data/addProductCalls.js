81bdafc570c4e789e7f2e1e4c6d04226109c2406
saul3d <saul3d@yahoo.com>
2019-12-02 17:48:20 -0600

    trying to remove duplicat folders

--- bookandcanvas.ui/src/Helpers/data/addProductCalls.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const baseUrl = "http://localhost:50090/api";
-
-const addNewProduct = (newProd) => axios.post(`${baseUrl}/product`, newProd);
-
-export default { addNewProduct };
