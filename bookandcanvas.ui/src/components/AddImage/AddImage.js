0354961ca9945ef2ffaa7f932fed9ac904e9f6f7
Silvestre Luna <slvtrluna@gmail.com>
2019-12-05 19:59:06 -0600

    styled input button

--- bookandcanvas.ui/src/components/AddImage/AddImage.js
+++ bookandcanvas.ui/src/components/AddImage/AddImage.js
@@ -1,43 +1,68 @@
 import React from 'react';
-import { Label, Input, Form } from 'reactstrap';
+import { Label, Input } from 'reactstrap';
 import data from '../../helpers/data/postImg';
 import './AddImage.scss';
 
 class AddImage extends React.Component {
   state = {
-    selectedImg: [],
-    img: '',
+    imgForImgbb: '',
+    tempLocalImgDisplay: '',
   }
 
   handleChange = (e) => {
     const img = e.target.files[0];
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      this.setState({ selectedImg: reader.result, img });
-    };
-    reader.readAsDataURL(img);
+    console.error(img);
+    if (img) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.setState({ tempLocalImgDisplay: reader.result, imgForImgbb: img });
+      };
+      reader.readAsDataURL(img);
+    }
   };
 
   uploadImg = () => {
     console.error('working?');
-    const imgTest = this.state.img;
+    const { imgForImgbb } = this.state;
     const dataForm = new FormData();
-    dataForm.append('image', imgTest);
+    dataForm.append('image', imgForImgbb);
     data.addImgToImgBB(dataForm)
       .then((resp) => console.error(resp.data))
       .catch((error) => console.error(error));
   }
 
   render() {
+    const { tempLocalImgDisplay } = this.state;
+
     return (
-      <React.Fragment>
-        <div>
-          <img src={this.state.selectedImg} />
+      <div className="AddImage">
+        <div className="diplay-temp-img">
+          {
+            (tempLocalImgDisplay)
+              ? (
+                <img src={this.state.tempLocalImgDisplay} alt="selected img" />
+              )
+              : (
+                ''
+              )
+          }
         </div>
-          <Label for="img-url">Upload Image</Label>
+        <div className="file-upload-input">
           <Input type="file" name="imgUrl" id="img-url" onChange={this.handleChange} />
-          <Input type="button" value="submit" id="upload" name="btn-to-upload" onClick={this.uploadImg} />
-      </React.Fragment>
+          <Label for="img-url">Select an Image to upload</Label>
+        </div>
+        {
+          (tempLocalImgDisplay)
+            ? (
+              <Input type="button" className="btn btn-primary" value="Upload Image" id="upload" name="btn-to-upload" onClick={this.uploadImg} />
+            )
+            : (
+              ''
+            )
+        }
+        <div>
+        </div>
+      </div>
     );
   }
 }
