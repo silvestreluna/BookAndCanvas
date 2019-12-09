de973103e858672021c8c47b700ec2bd665f2811
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-12-08 20:01:24 -0600

    updated the rest of the repos with the correct code

--- BookAndCanvas/Repositories/ProductRepo.cs
+++ BookAndCanvas/Repositories/ProductRepo.cs
@@ -6,30 +6,33 @@ using BookAndCanvas.DTOs;
 using BookAndCanvas.Models;
 using Dapper;
 
-namespace BookAndCanvas.Repositories {
-  public class ProductRepo: IProductRepository {
-    public Product AddNewProduct(NewProductDTO newProduct) {
-      using (var db = new SqlConnection(RepositoryConstants.ConnectionString)) {
-        var sql = @"INSERT INTO Product
+namespace BookAndCanvas.Repositories
+{
+    public class ProductRepo : IProductRepository
+    {
+        string _connectionString = "Server=localhost;Database=BookAndCanvas;Trusted_Connection=True";
+
+        public Product AddNewProduct(NewProductDTO newProduct)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"INSERT INTO Product
                             ([ServiceType], [Description], [SellerId], [ProductName], [Price], [ImgUrl], [Qty])
                             output inserted.*
                             VALUES(@ServiceType, @Description, @SellerId, @ProductName, @Price, @ImgUrl, @Qty)";
 
-        return db.QueryFirst<Product>(sql, newProduct);
-      }
-    }
-
-    public IEnumerable<Product> GetAllProducts() {
-      try {
-        using (var db = new SqlConnection(RepositoryConstants.ConnectionString)) {
-          var products = db.Query<Product>(@"Select * from Product");
-          return products.ToList();
+                return db.QueryFirst<Product>(sql, newProduct);
+            }
         }
-      } catch (Exception exception) {
-        Console.WriteLine($"Error getting products: {exception}");
-      }
 
-      return new List<Product>();
+        public IEnumerable<Product> GetAllProducts()
+        {
+
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var products = db.Query<Product>(@"Select * from Product");
+                return products.ToList();
+            }
+        }
     }
-  }
 }
--- BookAndCanvas/Repositories/ServiceRepo.cs
+++ BookAndCanvas/Repositories/ServiceRepo.cs
@@ -3,14 +3,21 @@ using System.Data.SqlClient;
 using BookAndCanvas.Models;
 using Dapper;
 
-namespace BookAndCanvas.Repositories {
-  public class ServiceRepo {
-    public IEnumerable<Service> GetAllServices() {
-      using (var db = new SqlConnection(RepositoryConstants.ConnectionString)) {
-        var sql = "SELECT * FROM [Service]";
+namespace BookAndCanvas.Repositories
+{
+    public class ServiceRepo
+    {
+        string _connectionString = "Server=localhost;Database=BookAndCanvas;Trusted_Connection=True";
 
-        return db.Query<Service>(sql);
-      }
+        public IEnumerable<Service> GetAllServices()
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = "SELECT * FROM [Service]";
+
+                return db.Query<Service>(sql);
+            }
+        }
+
     }
-  }
 }
