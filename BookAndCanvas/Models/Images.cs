d8c0658c6a97b975524a680285bd0656270bacff
Silvestre Luna <slvtrluna@gmail.com>
2019-12-15 15:44:37 -0600

    wip

---
+++ BookAndCanvas/Models/Images.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAndCanvas.Models
+{
+    public class Images
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ BookAndCanvas/Repositories/ImagesRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BookAndCanvas.Models;
+using Dapper;
+
+namespace BookAndCanvas.Repositories
+{
+    public class ImagesRepo
+    {
+        string _connectionString = "Server=localhost;Database=BookAndCanvas;Trusted_Connection=True";
+
+        public IEnumerable<Images> GetImages(int productId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"Select *
+                            from images
+                            where productId = @ProductId
+                          ";
+            
+                return db.Query<Images>(sql, new { productId });
+            }
+        }
+       
+    }
+}
